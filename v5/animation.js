e4b5b265b15737d934c09015e9186003f1ba6a08
dinthclex <dinthclex@gmail.com>
2025-04-17 19:22:02 -0300

    Add files via upload
    Alguns arquivos atualizados e também algumas correções.

---
+++ v5/animation.js
@@ -0,0 +1,99 @@
+//------------------ animation ------------------
+
+
+
+function animationInit(){
+	
+	//informações
+	infoIntemUncheck("legend");
+	infoIntemUncheck("equation");
+	infoIntemUncheck("title");
+	infoIntemUncheck("subtitle");
+	infoIntemUncheck("description");
+	infoIntemUncheck("originIndicator");
+	infoIntemUncheck("originAxis");
+	infoIntemUncheck("divisor");
+	infoIntemUncheck("worldOriginIndicator");
+	infoIntemUncheck("worldOriginAxis");
+	infoIntemUncheck("worldPoints");
+	//tela
+	infoIntemUncheck("screenXY");
+	infoIntemUncheck("screenOrigin");
+	infoIntemUncheck("      info.screenMeasure    ");
+	infoIntemUncheck("projectedPointsXY");
+	
+	//câmera em movimento
+	infoIntemUncheck("info.moveView.arrow");
+	infoIntemUncheck("info.moveView.axis");
+	infoIntemUncheck("info.moveView.transformedPoints");
+	infoIntemUncheck("info.moveView.lines");
+	infoIntemUncheck("info.moveView.projectedPoints");
+	infoIntemUncheck("info.moveView.triangles");
+	infoIntemUncheck("info.moveView.measures");
+	//grupo de botões (câmera em movimento)
+	infoGroupButUncheck("info.moveView.position");
+	infoGroupButUncheck("info.moveView.focalLength");
+	infoGroupButUncheck("info.moveView.projectionScreen");
+	infoGroupButUncheck("info.moveView.leftPoint");
+	infoGroupButUncheck("info.moveView.leftLine");
+	infoGroupButUncheck("info.moveView.leftRightAngle");
+	infoGroupButUncheck("info.moveView.rightPoint");
+	infoGroupButUncheck("info.moveView.rightLine");
+	infoGroupButUncheck("info.moveView.rightRightAngle");
+	infoGroupButUncheck("info.moveView.screen");
+	infoGroupButUncheck("info.moveView.fov");
+	
+	//câmera vista de topo
+	infoIntemUncheck("info.topView.arrow");
+	infoIntemUncheck("info.topView.axis");
+	infoIntemUncheck("info.topView.transformedPoints");
+	infoIntemUncheck("info.topView.lines");
+	infoIntemUncheck("info.topView.projectedPoints");
+	infoIntemUncheck("info.topView.triangles");
+	infoIntemUncheck("info.topView.measures");
+	//grupo de botões (vista de topo)
+	infoGroupButUncheck("info.topView.position");
+	infoGroupButUncheck("info.topView.focalLength");
+	infoGroupButUncheck("info.topView.projectionScreen");
+	infoGroupButUncheck("info.topView.leftPoint");
+	infoGroupButUncheck("info.topView.leftLine");
+	infoGroupButUncheck("info.topView.leftRightAngle");
+	infoGroupButUncheck("info.topView.rightPoint");
+	infoGroupButUncheck("info.topView.rightLine");
+	infoGroupButUncheck("info.topView.rightRightAngle");
+	infoGroupButUncheck("info.topView.screen");
+	infoGroupButUncheck("info.topView.fov");
+	
+	//câmera vista lateral
+	infoIntemUncheck("info.sideView.arrow");
+	infoIntemUncheck("info.sideView.axis");
+	infoIntemUncheck("info.sideView.transformedPoints");
+	infoIntemUncheck("info.sideView.lines");
+	infoIntemUncheck("info.sideView.projectedPoints");
+	infoIntemUncheck("info.sideView.triangles");
+	infoIntemUncheck("info.sideView.measures");
+	//grupo de botões (câmera vista lateral)
+	infoGroupButUncheck("info.sideView.position");
+	infoGroupButUncheck("info.sideView.focalLength");
+	infoGroupButUncheck("info.sideView.projectionScreen");
+	infoGroupButUncheck("info.sideView.leftPoint");
+	infoGroupButUncheck("info.sideView.leftLine");
+	infoGroupButUncheck("info.sideView.leftRightAngle");
+	infoGroupButUncheck("info.sideView.rightPoint");
+	infoGroupButUncheck("info.sideView.rightLine");
+	infoGroupButUncheck("info.sideView.rightRightAngle");
+	infoGroupButUncheck("info.sideView.screen");
+	infoGroupButUncheck("info.sideView.fov");
+	
+}
+
+
+function animationUpdate(){
+	
+	/* animação aqui */
+	
+}
+
+
+
+//------------------ animation ------------------
